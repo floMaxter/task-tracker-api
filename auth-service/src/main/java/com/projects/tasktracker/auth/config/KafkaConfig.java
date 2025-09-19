103017eecd72059f70c10ca0e0437e257ad8a692
Max Vdovin <maxpostupaet@gmail.com>
2025-09-19 21:53:26 +0300

    feat: add producer configuration using java config

---
+++ auth-service/src/main/java/com/projects/tasktracker/auth/config/KafkaConfig.java
@@ -0,0 +1,49 @@
+package com.projects.tasktracker.auth.config;
+
+import com.projects.tasktracker.auth.config.prop.KafkaProducerProperties;
+import com.projects.tasktracker.auth.kafka.event.UserWelcomeEmailEvent;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConfig {
+
+    private final KafkaProducerProperties kafkaProducerProperties;
+
+    Map<String, Object> producerConfig() {
+        var config = new HashMap<String, Object>();
+
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProducerProperties.getBootstrapServers());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaProducerProperties.getKeySerializer());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaProducerProperties.getValueSerializer());
+        config.put(ProducerConfig.ACKS_CONFIG, kafkaProducerProperties.getAcks());
+        config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, kafkaProducerProperties.getDeliveryTimeout());
+        config.put(ProducerConfig.LINGER_MS_CONFIG, kafkaProducerProperties.getLinger());
+        config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, kafkaProducerProperties.getRequestTimeout());
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, kafkaProducerProperties.getEnableIdempotence());
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, kafkaProducerProperties.getMaxInFlightRequestsPerConnection());
+        config.put(JsonSerializer.TYPE_MAPPINGS, kafkaProducerProperties.getSpringJsonTypeMapping());
+
+        return config;
+    }
+
+    @Bean
+    public ProducerFactory<String, UserWelcomeEmailEvent> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfig());
+    }
+
+    @Bean
+    public KafkaTemplate<String, UserWelcomeEmailEvent> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
---
+++ auth-service/src/main/java/com/projects/tasktracker/auth/config/prop/KafkaProducerProperties.java
@@ -0,0 +1,46 @@
+package com.projects.tasktracker.auth.config.prop;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "spring.kafka.producer")
+public class KafkaProducerProperties {
+
+    private String bootstrapServers;
+    private String keySerializer;
+    private String valueSerializer;
+    private String acks;
+    private Map<String, String> properties = new HashMap<>();
+
+    public String getDeliveryTimeout() {
+        return properties.get("delivery.timeout.ms");
+    }
+
+    public String getRequestTimeout() {
+        return properties.get("request.timeout.ms");
+    }
+
+    public String getLinger() {
+        return properties.get("linger.ms");
+    }
+
+    public String getEnableIdempotence() {
+        return properties.get("enable.idempotence");
+    }
+
+    public String getMaxInFlightRequestsPerConnection() {
+        return properties.get("max.in.flight.requests.per.connection");
+    }
+
+    public String getSpringJsonTypeMapping() {
+        return properties.get("spring.json.type.mapping");
+    }
+}
